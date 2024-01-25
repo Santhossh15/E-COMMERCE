51738305c99142da7f7cb68efa82e56e9182c573
Santhossh15 <santhossh15@gmail.com>
2024-01-25 18:41:58 +0530

    ShopContext && ShopCategory

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const LoginSignup = () => {
-  return <div>LoginSignup</div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1></h1>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
